3f8c0f24e899f30320ed8570c3655fbb63e9ec67
editid <me@dfarkas.uk>
2024-11-06 21:40:03 +0000

    Nov 6, 2024, 9:40 PM

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+tools = [
+    {
+        'name': 'Git',
+        'description': 'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
+        'link': 'https://git-scm.com/',
+        'image': 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png'
+    },
+]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tools=tools)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5738)
