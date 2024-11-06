6d46c1a2b8886e34f082b45ad075cab5e514360a
editid <me@dfarkas.uk>
2024-11-06 22:28:40 +0000

    Nov 6, 2024, 10:28 PM

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 tools = [
     {
-        'name': 'Git',
-        'description': 'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
-        'link': 'https://git-scm.com/',
-        'image': 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png'
+        'name': 'Hex to RGB and RGB to Hex',
+        'description': 'Converts hex to rgb and rgb to hex',
+        'link': '/hex2rgb',
+        'image': 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/haskell/haskell-original.svg'
     },
 ]
 
@@ -15,6 +15,7 @@ def index():
     return render_template('index.html', tools=tools)
 
 @app.route('/hex2rgb')
+@app.route('/rgb2hex')
 def hex2rgb():
     return render_template('hex2rgb.html')
 
