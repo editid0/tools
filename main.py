cb2508bc3e49f2c7eca47cbed989556bde80cae3
editid <me@dfarkas.uk>
2024-11-07 01:16:47 +0000

    Nov 7, 2024, 1:16 AM

--- main.py
+++ main.py
@@ -79,6 +79,9 @@ def colorai():
     colors = [c for c in colors if c.upper() != color.upper()]
     return {'reason': res, 'choices': colors}
 
+@app.route('/assets/<filename>')
+def serve_static(filename):
+    return app.send_static_file(filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5738)
