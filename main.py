fe4bf7a73e516b6da4d4960a0c700d7229471832
editid0 <66117959+editid0@users.noreply.github.com>
2024-11-09 20:39:27 +0000

    Added IP based limits, updated prompt, changed response format, added like and dislike routes, and added more debugging for .env file errors

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+from datetime import datetime, timedelta
+import hashlib
 import json
 from flask import Flask, render_template, request
 import requests, re
 import dotenv
 import os
 from openai import OpenAI
+import openai
 from pydantic import BaseModel
+import humanize
+
+# check if .env file exists
+if not os.path.exists(".env"):
+    # create .env file
+    with open(".env", "w") as f:
+        f.write("")
+
+    # add OPENAI_API_KEY to .env file
+    with open(".env", "a") as f:
+        f.write("OPENAI_API_KEY=''")
+
+# check if data.json exists
+if not os.path.exists("data.json"):
+    # create data.json file
+    with open("data.json", "w") as f:
+        f.write("{}")
 
 dotenv.load_dotenv()
 
+# Used for OpenAI
 class Color(BaseModel):
     reason: str
     choices: list[str]
@@ -126,27 +147,66 @@ def colorai():
     if len(color) != 7 or color[0] != "#":
         return {"reason": "Invalid color", "choices": []}
     print(f"color: {color}, fgbg: {fgbg}")
+    # print the user's ip if CF-Connecting-IP isn't set
+    if "CF-Connecting-IP" not in request.headers:
+        ip = request.remote_addr
+    else:
+        ip = request.headers["CF-Connecting-IP"]
+    # hash the ip
+    ip_hash = hashlib.sha256(ip.encode()).hexdigest()
+    # check if the ip has been seen before in data.json
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    today_string = datetime.now().strftime("%d-%m-%Y")
+    if today_string not in data:
+        data[today_string] = {}
+    if ip_hash in data[today_string]:
+        data[today_string][ip_hash] += 1
+    else:
+        data[today_string][ip_hash] = 1
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=4)
+    # check if the count is greater than 10
+    if data[today_string][ip_hash] > 10:
+        # create variable resets that is time until midnight of the next day
+        resets = (datetime.now() + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0) - datetime.now()
+        # convert to human readable format using humanize
+        resets = humanize.naturaldelta(resets, minimum_unit="seconds")
+        return {"reason": "You have exceeded the maximum number of attempts for today, your limit resets in " + resets + ".", "choices": ['#000000'], "remaining": 0}
+    else:
+        remaining = 10 - data[today_string][ip_hash]
     # return {'reason': 'Color not found', 'choices': []}
     # local AI
     # req = requests.get(f'http://192.168.7.254:57372?color={color}&fgbg={fgbg}')
     # return req.json()
     # openAI
-    chat_completion = client.beta.chat.completions.parse(
-        messages=[
-            {
-                "role": "system",
-                "content": "You are an AI designed to help pick out complementary colors that are accessible to users, depending on whether you are provided with a foreground (text) color, or a background color you will provide with a suitable choice for the other one, and provide a short sentence explaining why it's the ideal choice, as well as other possible choices if there are any. Output a reason and a list of colors in hex format.",
-            },
-            {
-                "role": "user",
-                "content": f"{fgbg} color is {color}",
-            },
-        ],
-        model="gpt-4o-mini",
-        response_format=Color
-    )
-    res = json.loads(chat_completion.choices[0].message.content)
-    return {'reason': res['reason'], 'choices': res['choices']}
+    try:
+        chat_completion = client.beta.chat.completions.parse(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an AI designed to help pick out complementary colors that are accessible to users, depending on whether you are provided with a foreground (text) color, or a background color you will provide with a suitable choice for the other one, and provide a short sentence explaining why it's the ideal choice, as well as other possible choices if there are any. Output a reason and a list of colors in hex format only. Do not provide any RGB values, do not provide color names.",
+                },
+                {
+                    "role": "user",
+                    "content": f"{fgbg} color is {color}",
+                },
+            ],
+            model="gpt-4o-mini",
+            response_format=Color
+        )
+        response = chat_completion.choices[0].message
+        if response.refusal:
+            return {'reason': 'Too many attempts. Please try again.', 'choices': ['#000000']}
+        res = json.loads(response.content)
+        choices = [c.upper().strip() for c in res['choices']]
+        print(choices)
+        return {'reason': res['reason'], 'choices': choices, 'remaining': remaining}
+    except Exception as e:
+        if type(e) == openai.LengthFinishReasonError:
+            return {'reason': 'Too many attempts. Please try again.', 'choices': ['#000000']}
+        else:
+            return {'reason': 'Something went wrong. Please try again.', 'choices': ['#000000']}
     # res = chat_completion.choices[0].message.content
     # colors = extract_hex_codes(res)
     # colors = [c for c in colors if c.upper() != color.upper()]
@@ -188,16 +248,25 @@ def hex2hsl():
 def diffeditor():
     return render_template("diffeditor.html")
 
+@app.route('/like', methods=['POST'])
+def like():
+    data = request.get_json()
+    print(data) # do something with the data at some point
+    return "OK"
 
-# REMOVE IN PRODUCTION
-# REMOVE IN PRODUCTION
-# REMOVE IN PRODUCTION
+@app.route('/dislike', methods=['POST'])
+def dislike():
+    data = request.get_json()
+    print(data) # do something with the data at some point
+    return "OK"
 
+# REMOVE BELOW IN PRODUCTION
+# REMOVE BELOW IN PRODUCTION
+# REMOVE BELOW IN PRODUCTION
 
 @app.route("/template/<template_name>")
 def template(template_name):
     return render_template(template_name + ".html")
-
 
 # REMOVE ABOVE IN PRODUCTION
 # REMOVE ABOVE IN PRODUCTION
