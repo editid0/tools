8c58f0945c2d582b92d15be8a6acad8e540642ef
editid0 <66117959+editid0@users.noreply.github.com>
2025-01-16 14:22:35 +0000

    not ready yet

--- tools.py
+++ tools.py
@@ -1,115 +1,148 @@
 if __name__ == "__main__":
-    print("#"*20)
-    print("#"*20)
+    print("#" * 20)
+    print("#" * 20)
     print("This file is not meant to be run directly.")
-    print("#"*20)
-    print("#"*20)
+    print("#" * 20)
+    print("#" * 20)
     print("Do not run this file.")
-    print("#"*20)
-    print("#"*20)
+    print("#" * 20)
+    print("#" * 20)
     exit()
 
-#https://stackoverflow.com/a/59155127/15622854
+# https://stackoverflow.com/a/59155127/15622854
 from flask import Blueprint, render_template, request
 
-tools_blueprint = Blueprint('tools', __name__)
+tools_blueprint = Blueprint("tools", __name__)
 
-@tools_blueprint.route('/ai_color_palette')
+
+@tools_blueprint.route("/ai_color_palette")
 def ai_color_palette():
-    return render_template('aicolorpalette.html')
+    return render_template("aicolorpalette.html")
 
-@tools_blueprint.route('/base_64_to_image')
+
+@tools_blueprint.route("/base_64_to_image")
 def base_64_to_image():
-    return render_template('base642image.html')
+    return render_template("base642image.html")
 
-@tools_blueprint.route('/diff_editor')
+
+@tools_blueprint.route("/diff_editor")
 def diff_editor():
-    return render_template('diffeditor.html')
+    return render_template("diffeditor.html")
 
-@tools_blueprint.route('/foreground_helper')
+
+@tools_blueprint.route("/foreground_helper")
 def foreground_helper():
-    return render_template('foreground.html', path=request.path)
+    return render_template("foreground.html", path=request.path)
 
-@tools_blueprint.route('/background_helper')
+
+@tools_blueprint.route("/background_helper")
 def background_helper():
-    return render_template('foreground.html', path=request.path)
+    return render_template("foreground.html", path=request.path)
 
-@tools_blueprint.route('/hex_to_hsl')
+
+@tools_blueprint.route("/hex_to_hsl")
 def hex_to_hsl():
-    return render_template('hex2hsl.html')
+    return render_template("hex2hsl.html")
 
-@tools_blueprint.route('/hex_to_rgb')
+
+@tools_blueprint.route("/hex_to_rgb")
 def hex_to_rgb():
-    return render_template('hex2rgb.html')
+    return render_template("hex2rgb.html")
 
-@tools_blueprint.route('/image_to_palette')
+
+@tools_blueprint.route("/image_to_palette")
 def image_to_palette():
-    return render_template('im2palette.html')
+    return render_template("im2palette.html")
 
-@tools_blueprint.route('/image_to_base64')
+
+@tools_blueprint.route("/image_to_base64")
 def image_to_base64():
-    return render_template('image2base64.html')
+    return render_template("image2base64.html")
 
-@tools_blueprint.route('/json_editor')
+
+@tools_blueprint.route("/json_editor")
 def json_editor():
-    return render_template('json_editor.html')
+    return render_template("json_editor.html")
 
-@tools_blueprint.route('/json_to_yaml')
+
+@tools_blueprint.route("/json_to_yaml")
 def json_to_yaml():
-    return render_template('json2yaml.html')
+    return render_template("json2yaml.html")
 
-@tools_blueprint.route('/markdown_editor')
+
+@tools_blueprint.route("/markdown_editor")
 def markdown_editor():
-    return render_template('markdownEditor.html')
+    return render_template("markdownEditor.html")
 
-@tools_blueprint.route('/meta_tag_generator')
+
+@tools_blueprint.route("/meta_tag_generator")
 def meta_tag_generator():
-    return render_template('metatags.html')
+    return render_template("metatags.html")
 
-@tools_blueprint.route('/palette_generator')
+
+@tools_blueprint.route("/palette_generator")
 def palette_generator():
-    return render_template('palettegen.html')
+    return render_template("palettegen.html")
 
-@tools_blueprint.route('/qr_code_generator')
+
+@tools_blueprint.route("/qr_code_generator")
 def qr_code_generator():
-    return render_template('qrcodegen.html')
+    return render_template("qrcodegen.html")
 
-@tools_blueprint.route('/regex_generator')
+
+@tools_blueprint.route("/regex_generator")
 def regex_generator():
-    return render_template('regexgenerator.html')
+    return render_template("regexgenerator.html")
 
-@tools_blueprint.route('/svg_to_image')
+
+@tools_blueprint.route("/svg_to_image")
 def svg_to_image():
-    return render_template('svg2image.html')
+    return render_template("svg2image.html")
 
-@tools_blueprint.route('/timestamp_converter')
+
+@tools_blueprint.route("/timestamp_converter")
 def timestamp_converter():
-    return render_template('timestampconv.html')
+    return render_template("timestampconv.html")
 
-@tools_blueprint.route('/uuid_generator')
+
+@tools_blueprint.route("/uuid_generator")
 def uuid_generator():
-    return render_template('uuidgen.html')
+    return render_template("uuidgen.html")
 
-@tools_blueprint.route('/password_generator')
+
+@tools_blueprint.route("/password_generator")
 def password_generator():
-    return render_template('passwordgen.html')
+    return render_template("passwordgen.html")
+
 
 @tools_blueprint.route("/socketio")
 def socketio():
     return render_template("redirto_socketio.html")
 
+
 @tools_blueprint.route("/clipboard_to_image")
 def clipboard_to_image():
     return render_template("clipboard2file.html")
 
-@tools_blueprint.route('/lorem_ipsum_generator')
+
+@tools_blueprint.route("/lorem_ipsum_generator")
 def lorem_ipsum_generator():
-    return render_template('loremipsum.html')
+    return render_template("loremipsum.html")
 
-@tools_blueprint.route('/url_encode_decode')
+
+@tools_blueprint.route("/url_encode_decode")
 def url_encode_decode():
-    return render_template('urlencdec.html')
+    return render_template("urlencdec.html")
 
-@tools_blueprint.route('/hash_generator')
+
+@tools_blueprint.route("/hash_generator")
 def hash_generator():
-    return render_template('hasher.html')
+    return render_template("hasher.html")
+
+
+@tools_blueprint.route("/aiv3")
+def aiv3():
+    """
+    Use chatgpt memory function to hardcode the coventry college colors
+    """
+    return render_template("aicolor2.html")
